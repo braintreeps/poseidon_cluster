2d291eee25a6c67cb8e517c702ec88db1b86525b
cory <cory.monty@gmail.com>
2014-09-25 13:58:19 +0000

    Stops trying to call private method on Poseidon::TopicMetadata

--- lib/poseidon/consumer_group.rb
+++ lib/poseidon/consumer_group.rb
@@ -190,7 +190,7 @@ class Poseidon::ConsumerGroup
   def partitions
     return [] unless topic_metadata
 
-    topic_metadata.partitions.sort_by do |part|
+    topic_metadata.struct.partitions.sort_by do |part|
       broker = metadata.brokers[part.leader]
       [broker.host, broker.port].join(":")
     end
